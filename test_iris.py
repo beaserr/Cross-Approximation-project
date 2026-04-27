348c8fa3388ffc43d5d5e776610c67c3e5923ba5
beaserr <161707198+beaserr@users.noreply.github.com>
2026-04-27 22:38:30 +0200

    Update test_iris.py

--- test_iris.py
+++ test_iris.py
@@ -9,10 +9,8 @@ def run_iris_test(max_rank=40):
     X, y, data = load_iris_data()
 
     K = linear_kernel_matrix(X)
-
     ca_full, _, _ = fpCA(K, max_rank)
     ca_partial, _, _ = ppCA(K, max_rank, start_row=0)
-
     r = min(len(ca_full), len(ca_partial), max_rank)
     svd_err = frob_error_svd(K, r)
 
@@ -22,7 +20,6 @@ def run_iris_test(max_rank=40):
     plt.semilogy(ranks, svd_err, label="SVD")
     plt.semilogy(np.arange(1, len(ca_full) + 1), ca_full, label="FCA")
     plt.semilogy(np.arange(1, len(ca_partial) + 1), ca_partial, label="ppCA")
-
     plt.title("Iris kernel matrix approximation")
     plt.xlabel("Rank")
     plt.ylabel("Relative Frobenius error")
